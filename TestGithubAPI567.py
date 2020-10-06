d842ced5ac36115e71574000d9369feca95e6077
mfahim5 <33581164+mfahim5@users.noreply.github.com>
2020-10-06 13:54:06 -0400

    add mock

--- TestGithubAPI567.py
+++ TestGithubAPI567.py
@@ -1,18 +1,25 @@
 import unittest
 from GithubAPI567 import githubAPI
 from unittest import mock
+from unittest.mock import MagicMock
 
 class TestGithubAPI567(unittest.TestCase):
-    def testWorkingUserInput(self):
-        self.assertEqual(githubAPI('mfahim5'), True)
+    @mock.patch('GithubAPI567.githubAPI')
+    def testWorkingUserInputMock(self,mockrequirements):
+        mockrequirements.return_value.json.return_value= ('mfahim5')
+        res=githubAPI("mfahim5")
+        self.assertEqual(res, True)
     
-    def testNonWorkingUserInput1(self):
+    # def testWorkingUserInputMock1(self,mockrequirements):
+    #     mockrequirements.return_value.status_code= ('Repo Name: Complexity    Number of commits: 30 Repo Name: Final_Project_SW555   Number of commits: 30 Repo Name: FocusBot      Number of commits: 8 Repo Name: Hoboken-Happening     Number of commits: 1 Repo Name: Personal-Website      Number of commits: 7 Repo Name: SSW-345       Number of commits: 28 Repo Name: SSW567-HW4a   Number of commits: 26 Repo Name: test-HW4-345  Number of commits: 2 Repo Name: test-SSW-345  Number of commits: 2 Repo Name: TriangleTest  Number of commits: 12')
+    #     res=githubAPI("mfahim5")
+    #     self.assertEqual(res, 'Repo Name: Complexity    Number of commits: 30 Repo Name: Final_Project_SW555   Number of commits: 30 Repo Name: FocusBot      Number of commits: 8 Repo Name: Hoboken-Happening     Number of commits: 1 Repo Name: Personal-Website      Number of commits: 7 Repo Name: SSW-345       Number of commits: 28 Repo Name: SSW567-HW4a   Number of commits: 26 Repo Name: test-HW4-345  Number of commits: 2 Repo Name: test-SSW-345  Number of commits: 2 Repo Name: TriangleTest  Number of commits: 12' )
+    def testNonWorkingUserInputMock1(self):
         self.assertEqual(githubAPI(000), 'Github account must be written correctly', 'Strings are only allowed in githubAPI')   
-    def testNonWorkingUserInput2(self):
-        self.assertEqual(githubAPI("HIIIII"), False)
-    def testNonWorkingUserInput3(self):
-        self.assertEqual(githubAPI(11), 'Github account must be written correctly', 'Strings are only allowed in githubAPI')  
-    def testNonWorkingUserInput4(self):
+    def testNonWorkingUserInputMock2(self):
+        self.assertEqual(githubAPI("HIII"), False)
+
+    def testNonWorkingUserInputMock3(self):
         self.assertEqual(githubAPI(9999), 'Github account must be written correctly', 'Strings are only allowed in githubAPI')       
 
 if __name__ =="__main__":
